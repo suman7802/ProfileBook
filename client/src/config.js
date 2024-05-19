e1885b1da9a15d354d68be249cb3e00e799aab1e
suman7802 <mansu7802@gmail.com>
2024-05-19 13:36:12 +0545

    ready to host

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,24 @@
-const url = 'http://localhost:8000/api';
+/*
+ *
+ * use this line if you want to run on production
+ *
+ * */
 
-const cookieName = 'profileBook'; // must be the same as in the server
+const url = '/api';
+
+/*
+ *
+ * use this line if you want to run on development
+ *
+ * */
+
+// const url = 'http://localhost:8000/api';
+
+/*
+ *
+ * must be the same as in the server
+ *
+ * */
+const cookieName = 'profileBook';
 
 export { url, cookieName };
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 import App from './App';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,10 +7,8 @@ import { StatusProvider } from './context/status.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <StatusProvider>
-      <App />
-      <ToastContainer position="bottom-right" />
-    </StatusProvider>
-  </StrictMode>
+  <StatusProvider>
+    <App />
+    <ToastContainer position="bottom-right" />
+  </StatusProvider>
 );
