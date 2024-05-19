dd8c840e1fa0e462ff45268688ffd6f4c639b7d5
suman7802 <mansu7802@gmail.com>
2024-05-19 10:00:31 +0545

    worked on css of users card

--- client/src/components/ListUsers.jsx
+++ client/src/components/ListUsers.jsx
@@ -38,26 +38,28 @@ export default function ListUsers() {
 function User({ user }) {
   const [show, setShow] = useState(false);
   return (
-    <div className="user flex flex-col items-center  bg-[#ececece0] my-1 px-5 py-3 rounded-md">
-      <div className="flex flex-row gap-7 items-center">
+    <div className="user flex flex-col items-center  bg-[#ececece0] my-1 px-5 py-3 rounded-md w-full">
+      <div className="flex flex-row justify-between items-center w-full">
         <img
           src={user.profile}
           alt={user.fullName}
           className="w-16 h-16 object-cover rounded-full border-2 border-indigo-500"
         />
 
-        <div className="info">
-          <h2>{user.fullName}</h2>
-          <p className="text-sm">{user.email}</p>
-          <div className="status flex flex-row">
-            {user.verified && <p className="text-sm text-green-500">verified</p>}
-            <p className="text-sm text-green-500 lowercase">&nbsp;{user.role}&nbsp;&nbsp;</p>
-            <p
-              onClick={() => setShow(!show)}
-              className="text-sm text-blue-500 hover:cursor-pointer hover:underline"
-            >
-              more
-            </p>
+        <div className="textInfo flex flex-row justify-between w-[calc(100%-8rem)]">
+          <div className="info">
+            <h2>{user.fullName}</h2>
+            <p className="text-sm">{user.email}</p>
+            <div className="status flex flex-row">
+              {user.verified && <p className="text-sm text-green-500">verified</p>}
+              <p className="text-sm text-green-500 lowercase">&nbsp;{user.role}&nbsp;&nbsp;</p>
+              <p
+                onClick={() => setShow(!show)}
+                className="text-sm text-blue-500 hover:cursor-pointer hover:underline"
+              >
+                more
+              </p>
+            </div>
           </div>
         </div>
 
